01638694fd658c53bc05e5f81dc6ce5e4c475016
Joseph Parker <44680902+bleckfox@users.noreply.github.com>
2020-08-31 18:17:52 +0500

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -19,7 +19,7 @@ function Chat ( { location } ) {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState( [] );
-  const localhost = 'localhost:3030';
+  const localhost = 'localhost:5000';
 
 
   useEffect( () => {
